325f3c47b50fb03e5543bac7f7330523146402c0
Nathan Kleekamp <nkleekamp@gmail.com>
2012-11-28 22:52:03 -0500

    switching to flexible layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+(function() {
+    var window_size = null;
+    var getDocHeight = function() {
+        return Math.max(
+            Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
+            Math.max(document.body.offsetHeight, document.documentElement.offsetHeight),
+            Math.max(document.body.clientHeight, document.documentElement.clientHeight)
+        );
+    };
 
+    var setMainHeight = function() {
+        var window_size = getDocHeight() - 160 +'px';
+        return document.getElementById('main').style.height = window_size;
+    };
+
+    setMainHeight();
+
+    window.onresize = function() {
+        setMainHeight();
+    };
+
+})();
