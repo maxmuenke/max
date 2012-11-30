3deca571a6204a4bc2571c3309e3b772957868e6
Nathan Kleekamp <nkleekamp@gmail.com>
2012-11-29 22:49:52 -0500

    working on flexible layout

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 (function() {
-    var window_size = null;
-    var getDocHeight = function() {
-        return Math.max(
-            Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
-            Math.max(document.body.offsetHeight, document.documentElement.offsetHeight),
-            Math.max(document.body.clientHeight, document.documentElement.clientHeight)
-        );
-    };
-
-    var setMainHeight = function() {
-        var window_size = getDocHeight() - 160 +'px';
-        return document.getElementById('main').style.height = window_size;
-    };
-
-    setMainHeight();
+    var main = document.getElementById('main');
+    main.style.height = window.innerHeight - 279 + 'px';
 
     window.onresize = function() {
-        setMainHeight();
+        document.body.style.overflow = 'hidden';
+        main.style.height = window.innerHeight - 279 + 'px';
+        document.body.style.overflow = 'visible';
     };
 
 })();
+
+// Can use this to set #main to the length of text
+// main.style.height = main.scrollHeight + 'px';
